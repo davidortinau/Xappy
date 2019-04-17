fe4af2083969e0f985ce78442e6a6d48b4283717
David Ortinau <david.ortinau@microsoft.com>
2019-04-16 23:41:14 -0500

    starting a settings service to handle the app config.

--- Xappy/Xappy/Pages/MainPage.xaml.cs
+++ Xappy/Xappy/Pages/MainPage.xaml.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xappy.Services;
 
-namespace Xappy
+namespace Xappy.Pages
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
@@ -16,6 +17,17 @@ namespace Xappy
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            SettingsService.UseFlyout(this);
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            SettingsService.UseTabs(this);
         }
     }
 }
---
+++ Xappy/Xappy/Services/SettingsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xappy.Services
+{
+    public static class SettingsService
+    {
+        public static void UseTabs(BindableObject context)
+        {
+            Shell.SetFlyoutBehavior(Shell.Current, Xamarin.Forms.FlyoutBehavior.Disabled);
+            Shell.SetTabBarIsVisible(context, true);
+        }
+
+        public static void UseFlyout(BindableObject context)
+        {
+            Shell.SetFlyoutBehavior(Shell.Current, Xamarin.Forms.FlyoutBehavior.Flyout);
+            Shell.SetTabBarIsVisible(context, false);
+        }
+    }
+}
