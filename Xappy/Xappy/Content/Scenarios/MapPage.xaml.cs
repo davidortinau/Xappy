7583d1172823d6f4482c68d14446d16d81a9fa1c
sthewissen <github@thewissen.io>
2019-04-25 19:27:52 +0200

    Maps screen as shown on stream

--- Xappy/Xappy/Content/Scenarios/MapPage.xaml.cs
+++ Xappy/Xappy/Content/Scenarios/MapPage.xaml.cs
@@ -1,27 +1,59 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Xappy.Scenarios
 {
     public partial class MapPage : ContentPage
     {
+
+        private Thickness _safeInsets;
+
+        public void Handle_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    InformationPanel.TranslationY = Math.Max(_safeInsets.Top, 
+                        Math.Min(AdditionalContent.HeightRequest, InformationPanel.TranslationY + e.TotalY));
+                    break;
+            }
+        }
+
         public MapPage()
         {
             InitializeComponent();
-
-
             SetLocation();
-
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
+            _safeInsets = On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets();
 
+            AdditionalContent.HeightRequest = this.Height - MainContent.Height;
+            InformationPanel.TranslationY = AdditionalContent.HeightRequest;
+
+            var newPadding = InformationPanel.Padding;
+            newPadding.Bottom = _safeInsets.Bottom + newPadding.Bottom;
+            InformationPanel.Padding = newPadding;
+        }
+
+        public async void Handle_Tapped(object sender, EventArgs e)
+        {
+            await InformationPanel.TranslateTo(0, MainContent.HeightRequest, 400, Easing.CubicInOut);
+        }
+
+        public async void Handle_Clicked(object sender, EventArgs e)
+        {
+            var location = new Location(42.349344, -71.082504);
+            var options = new MapLaunchOptions { Name = "Wired Puppy", NavigationMode = NavigationMode.Driving };
+
+            await Xamarin.Essentials.Map.OpenAsync(location, options);
         }
 
         async void SetLocation()
@@ -33,10 +65,8 @@ namespace Xappy.Scenarios
                 if (location != null)
                 {
                     Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-                    MyMap.MoveToRegion(
-                MapSpan.FromCenterAndRadius(
-                    new Position(42.349344, -71.082504),
-                    Distance.FromMiles(0.3)));
+
+                    MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(42.349344, -71.082504), Distance.FromMiles(0.1)));
                 }
             }
             catch (FeatureNotSupportedException fnsEx)
