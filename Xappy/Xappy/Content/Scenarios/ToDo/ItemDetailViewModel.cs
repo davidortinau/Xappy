41000c609d6164a472c8015e569f5e9d274407cb
David Ortinau <david.ortinau@microsoft.com>
2019-05-03 10:25:51 -0500

    stubbed out todo with template code

---
+++ Xappy/Xappy/Content/Scenarios/ToDo/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XappyDo.Models;
+
+namespace XappyDo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
