ff3299b94bfc72b0790b80f8da8b4759f9e08682
David Ortinau <david.ortinau@microsoft.com>
2019-06-02 20:26:50 -0500

    Updating settings page
    Stubbed out UI for basic controls

---
+++ Xappy/Xappy/Content/Settings/ThemeCardView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Xappy.Content.Settings
+{
+    public partial class ThemeCardView : Frame
+    {
+        public ThemeCardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
