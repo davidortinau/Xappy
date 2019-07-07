9d40631664af97297d08179afa3276f15568c54c
David Ortinau <david.ortinau@microsoft.com>
2019-07-07 17:13:31 -0500

    finished parallax and header fade

---
+++ Xappy/Xappy/Converters/ComparisonConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Xappy.Converters
+{
+
+    public class ComparisonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fmt = new NumberFormatInfo();
+            fmt.NegativeSign = "-";
+
+            var allParams = ((string)parameter).Split((';'));
+            var compValue = Double.Parse(allParams[0], fmt);
+            var comparison = allParams[1];
+
+            switch (comparison)
+            {
+                case "<":
+                    return ((double)value) < compValue;
+                case ">":
+                    return ((double)value) > compValue;
+                case "!=":
+                    return ((double)value) != compValue;
+                case "==":
+                default:
+                    return ((double)value) == compValue;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
