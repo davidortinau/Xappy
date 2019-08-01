a96cb578edb097ed8cad14c3c69223cd40c88806
David Ortinau <david.ortinau@microsoft.com>
2019-08-01 15:42:37 -0500

    grouped collectionview for control index

--- Xappy/Xappy/Content/ControlGallery/IndexPageViewModel.cs
+++ Xappy/Xappy/Content/ControlGallery/IndexPageViewModel.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xappy.Content;
 using Xappy.Content.ControlGallery.ControlTemplates;
 
@@ -21,9 +23,11 @@ namespace Xappy.ControlGallery
 
         public List<ControlType> Views { get; set; }
 
+        public List<ControlGroup> XamarinAll { get; set; }
+
         public IndexPageViewModel()
         {
-            Layouts = new List<ControlType>
+            var layouts = new ControlGroup("Layouts", Color.Red, new List<ControlType>
             {
                 new ControlType{ Title = nameof(StackLayout), ControlTemplate = nameof(StackLayoutControlTemplate) },
                 new ControlType{ Title = nameof(Frame)},
@@ -35,9 +39,9 @@ namespace Xappy.ControlGallery
                 new ControlType{ Title = nameof(ContentView)},
                 new ControlType{ Title = nameof(ScrollView)},
                 new ControlType{ Title = nameof(TemplatedView)}
-            };
+            });;
 
-            Pages = new List<ControlType>
+            var pages = new ControlGroup("Pages", Color.Blue, new List<ControlType>
             {
                 new ControlType{ Title = nameof(ContentPage)},
                 new ControlType{ Title = nameof(TabbedPage)},
@@ -45,9 +49,9 @@ namespace Xappy.ControlGallery
                 new ControlType{ Title = nameof(NavigationPage)},
                 new ControlType{ Title = nameof(CarouselPage)}
 
-            };
+            });
 
-            Views = new List<ControlType>
+            var views = new ControlGroup("Views", Color.Orange, new List<ControlType>
             {
                 new ControlType{ Title = nameof(ActivityIndicator)},
                 new ControlType{ Title = nameof(Button)},
@@ -69,7 +73,12 @@ namespace Xappy.ControlGallery
                 new ControlType{ Title = nameof(WebView)}
 
 
-            };
+            });
+
+            XamarinAll = new List<ControlGroup>();
+            XamarinAll.Add(layouts);
+            XamarinAll.Add(pages);
+            XamarinAll.Add(views);
 
 
             SelectCommand = new Command(ControlSelected);
@@ -87,9 +96,236 @@ namespace Xappy.ControlGallery
         }
     }
 
+    public class ControlGroup : List<ControlType>
+    {
+        public ControlGroup(string name, Color color, List<ControlType> members) : base(members)
+        {
+            Name = name;
+            GroupColor = color;
+        }
+
+        public string Name { get; set; }
+
+        public Color GroupColor { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class Team : List<Member>
+    {
+        public Team(string name, List<Member> members) : base(members)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class Member
+    {
+        public Member(string name) => Name = name;
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class SuperTeams : List<Team>
+    {
+        public SuperTeams()
+        {
+            Add(new Team("Avengers",
+                new List<Member>
+                {
+                    new Member("Thor"),
+                    new Member("Captain America"),
+                    new Member("Iron Man"),
+                    new Member("The Hulk"),
+                    new Member("Ant-Man"),
+                    new Member("Wasp"),
+                    new Member("Hawkeye"),
+                    new Member("Black Panther"),
+                    new Member("Black Widow"),
+                    new Member("Doctor Druid"),
+                    new Member("She-Hulk"),
+                    new Member("Mockingbird"),
+                }
+            ));
+
+            Add(new Team("Fantastic Four",
+                new List<Member>
+                {
+                    new Member("The Thing"),
+                    new Member("The Human Torch"),
+                    new Member("The Invisible Woman"),
+                    new Member("Mr. Fantastic"),
+                }
+            ));
+
+            Add(new Team("Defenders",
+                new List<Member>
+                {
+                    new Member("Doctor Strange"),
+                    new Member("Namor"),
+                    new Member("Hulk"),
+                    new Member("Silver Surfer"),
+                    new Member("Hellcat"),
+                    new Member("Nighthawk"),
+                    new Member("Yellowjacket"),
+                }
+            ));
+
+            Add(new Team("Heroes for Hire",
+                new List<Member>
+                {
+                    new Member("Luke Cage"),
+                    new Member("Iron Fist"),
+                    new Member("Misty Knight"),
+                    new Member("Colleen Wing"),
+                    new Member("Shang-Chi"),
+                }
+            ));
+
+            Add(new Team("West Coast Avengers",
+                new List<Member>
+                {
+                    new Member("Hawkeye"),
+                    new Member("Mockingbird"),
+                    new Member("War Machine"),
+                    new Member("Wonder Man"),
+                    new Member("Tigra"),
+                }
+            ));
+
+            Add(new Team("Great Lakes Avengers",
+                new List<Member>
+                {
+                    new Member("Squirrel Girl"),
+                    new Member("Dinah Soar"),
+                    new Member("Mr. Immortal"),
+                    new Member("Flatman"),
+                    new Member("Doorman"),
+                }
+            ));
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class ObservableTeam : ObservableCollection<Member>
+    {
+        public ObservableTeam(string name, List<Member> members) : base(members)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [Preserve(AllMembers = true)]
+    public class ObservableSuperTeams : ObservableCollection<ObservableTeam>
+    {
+        public ObservableSuperTeams()
+        {
+            Add(new ObservableTeam("Avengers",
+                new List<Member>
+                {
+                    new Member("Thor"),
+                    new Member("Captain America"),
+                    new Member("Iron Man"),
+                    new Member("The Hulk"),
+                    new Member("Ant-Man"),
+                    new Member("Wasp"),
+                    new Member("Hawkeye"),
+                    new Member("Black Panther"),
+                    new Member("Black Widow"),
+                    new Member("Doctor Druid"),
+                    new Member("She-Hulk"),
+                    new Member("Mockingbird"),
+                }
+            ));
+
+            Add(new ObservableTeam("Fantastic Four",
+                new List<Member>
+                {
+                    new Member("The Thing"),
+                    new Member("The Human Torch"),
+                    new Member("The Invisible Woman"),
+                    new Member("Mr. Fantastic"),
+                }
+            ));
+
+            Add(new ObservableTeam("Defenders",
+                new List<Member>
+                {
+                    new Member("Doctor Strange"),
+                    new Member("Namor"),
+                    new Member("Hulk"),
+                    new Member("Silver Surfer"),
+                    new Member("Hellcat"),
+                    new Member("Nighthawk"),
+                    new Member("Yellowjacket"),
+                }
+            ));
+
+            Add(new ObservableTeam("Heroes for Hire",
+                new List<Member>
+                {
+                    new Member("Luke Cage"),
+                    new Member("Iron Fist"),
+                    new Member("Misty Knight"),
+                    new Member("Colleen Wing"),
+                    new Member("Shang-Chi"),
+                }
+            ));
+
+            Add(new ObservableTeam("West Coast Avengers",
+                new List<Member>
+                {
+                    new Member("Hawkeye"),
+                    new Member("Mockingbird"),
+                    new Member("War Machine"),
+                    new Member("Wonder Man"),
+                    new Member("Tigra"),
+                }
+            ));
+
+            Add(new ObservableTeam("Great Lakes Avengers",
+                new List<Member>
+                {
+                    new Member("Squirrel Girl"),
+                    new Member("Dinah Soar"),
+                    new Member("Mr. Immortal"),
+                    new Member("Flatman"),
+                    new Member("Doorman"),
+                }
+            ));
+        }
+    }
+
     public class ControlType
     {
         public string Title { get; set; }
         public string ControlTemplate { get; set; }
     }
+
+
 }
---
+++ Xappy/Xappy/Converters/IndexToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xappy.Converters
+{
+    public class IndexToColorConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var groupIndex = (int)value;
+
+            var allParams = ((string)parameter).Split((';'));
+
+            return LookupColor(allParams[groupIndex]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+
+        public Color LookupColor(string key)
+        {
+            try
+            {
+                Application.Current.Resources.TryGetValue(key, out var newColor);
+                return (Color)newColor;
+            }
+            catch
+            {
+                return Color.White;
+            }
+        }
+    }
+}
